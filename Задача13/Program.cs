408b2843881c12ec5fb0767fd2fba902179e5510
MikeDark <mkfist2911@gmail.com>
2022-10-16 23:57:54 +0300

    задача решена

---
+++ Задача13/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+ Console.WriteLine("введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number > 99)
+ {
+     string number1 = "";
+     number1 = Convert.ToString(number);
+     Console.WriteLine(number1[2]);
+ }
+ else
+ {
+     Console.WriteLine("This number is not have thirdiget");
+ }
