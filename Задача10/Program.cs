95f63b221da785afa70ae37abb5a5d15164879cd
MikeDark <mkfist2911@gmail.com>
2022-10-16 23:54:29 +0300

    задача решена

---
+++ Задача10/Program.cs
@@ -0,0 +1,11 @@
+﻿//// Напишите программу, которая принимает на вход трёхзначное число 
+////и на выходе показывает вторую цифру этого числа.
+
+
+Console.WriteLine("введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ int number1 = number / 10;
+ int number2 = number1 % 10;
+
+ Console.WriteLine(number2);
