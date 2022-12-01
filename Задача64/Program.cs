83d57018870e327432c662e41c0bdee3e21e4246
MikeDark <mkfist2911@gmail.com>
2022-12-01 19:21:25 +0300

    программа работает

--- Задача64/Program.cs
+++ Задача64/Program.cs
@@ -7,7 +7,6 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 Console.WriteLine("Введите натуральное число больше 1:");
 int number = int.Parse(Console.ReadLine());
 
-///Метод вывода натуральных чисел от N до 1:
 void NumberCounter (int number)
 {
     if (number < 0) Console.Write($"{number} не натуральное число");
